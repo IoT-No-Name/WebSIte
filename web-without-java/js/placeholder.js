6a65fe163a42cfe9976e1fc680fbbb5d49d81c6f
Nazar <nazarko1805@gmail.com>
2017-04-06 19:50:27 +0300

    [+] Main page and login page fixes

--- web-without-java/js/placeholder.js
+++ web-without-java/js/placeholder.js
@@ -1,8 +1,8 @@
 
 $(document).ready(function(){
-	
-	$('input[type="text"], input[type="password"], textarea').each(function() {
+
+	$('input[type="text"], input[type="password"], input[type="radio"], textarea').each(function() {
 		$(this).val( $(this).attr('placeholder') );
     });
-	
+
 });
