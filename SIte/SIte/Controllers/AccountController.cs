113269d309ff090212686bab6d301cbb6128cee9
Vova0199 <vova0143@gmail.com>
2017-04-26 18:28:35 +0300

    Add roles

--- SIte/SIte/Controllers/AccountController.cs
+++ SIte/SIte/Controllers/AccountController.cs
@@ -151,29 +151,20 @@ namespace SIte.Controllers
         {
             if (ModelState.IsValid)
             {
-                var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
-                var result = await UserManager.CreateAsync(user, model.Password);
+                var user = new ApplicationUser { UserName = model.Email.Split('@')[0],
+                                                Email = model.Email };
+                var result = await UserManager.CreateAsync(user, model.Password);             
                 if (result.Succeeded)
                 {
+                    UserManager.AddToRole(user.Id, model.TypeOfUser.ToString());
                     await SignInManager.SignInAsync(user, isPersistent:false, rememberBrowser:false);
-                    
-                    // For more information on how to enable account confirmation and password reset please visit https://go.microsoft.com/fwlink/?LinkID=320771
-                    // Send an email with this link
-                    // string code = await UserManager.GenerateEmailConfirmationTokenAsync(user.Id);
-                    // var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
-                    // await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
-
                     return RedirectToAction("Index", "Home");
                 }
                 AddErrors(result);
             }
-
-            // If we got this far, something failed, redisplay form
             return View(model);
         }
 
-        //
-        // GET: /Account/ConfirmEmail
         [AllowAnonymous]
         public async Task<ActionResult> ConfirmEmail(string userId, string code)
         {
