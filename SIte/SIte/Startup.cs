498a3f5a52deb7a1045ed385a6b9f46510fd4231
Vova0199 <vova0143@gmail.com>
2017-04-26 18:49:47 +0300

    Add roles

--- SIte/SIte/Startup.cs
+++ SIte/SIte/Startup.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.Owin;
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin;
 using Owin;
+using SIte.Models;
 
 [assembly: OwinStartupAttribute(typeof(SIte.Startup))]
 namespace SIte
@@ -9,6 +12,22 @@ namespace SIte
         public void Configuration(IAppBuilder app)
         {
             ConfigureAuth(app);
+            CreateRolesandUsers();
+        }
+        
+        private void CreateRolesandUsers()
+        {
+            ApplicationDbContext context = new ApplicationDbContext();
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+            if (!roleManager.RoleExists("Customer"))
+            { 
+                var role1 = new Microsoft.AspNet.Identity.EntityFramework.IdentityRole();
+                role1.Name = "Customer";
+                roleManager.Create(role1);
+                var role2 = new Microsoft.AspNet.Identity.EntityFramework.IdentityRole();
+                role2.Name = "Freelancer";
+                roleManager.Create(role2);
+            }
         }
     }
 }
